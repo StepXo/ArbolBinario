7e5c58f4ca07d059af2c260c3ebb6a79f927c083
StepXo <66577156+StepXo@users.noreply.github.com>
2023-06-05 16:49:36 -0500

    clase árbol binario cuasi completa

--- ArblolBinario.py
+++ ArblolBinario.py
@@ -1,32 +1,51 @@
-class Nodo():
+class Nodo(): #clase "auxiliar" para guardar los nodos y su informacion, asi como en las diapositivas del profe
     def __init__ (self, valor,padre = None):
         self.valor = valor
         self.padre = padre
         self.izquierda = None
         self.derecha = None
+        self.h = 0
     def __str__(self):
         return str(self.valor)
 
 class ArbolBinario():
 
     def __init__(self,raiz=None):
-        self.raiz = raiz
         self.len = 0
+        self.h = 0
+        self._listaRetorno = []
+        self._hojas = 0
+        self._hijos = 0
+        self._racimo = 0
+        if raiz == None:
+            self.raiz = None
+        elif type(raiz) == int:
+            self.raiz = None
+        else:
+            self.raiz = None
+            self.insertar(raiz)
+        
 
-    def insertar(self, valor):
+    def _insertarAux(self, valor):
         Nuevo = Nodo(valor)
         self.len+=1
 
         if self.len == 1:
-            self.raiz = Nuevo  
+            self.raiz = Nuevo
+            self.h = 1
+            Nuevo.h = 1
             return False  
 
         padre = self.raiz
+        aux = 2 #1 de la raiz y 1 porque el nodo no cuenta la vez que pasa por el hijo
         while True:
 
             if Nuevo.valor < padre.valor and padre.izquierda == None:
                 Nuevo.padre = padre
                 padre.izquierda = Nuevo
+                Nuevo.h = aux
+                if aux>self.h:
+                    self.h = aux
                 return True
 
             elif Nuevo.valor < padre.valor:
@@ -35,6 +54,9 @@ class ArbolBinario():
             elif Nuevo.valor > padre.valor and padre.derecha == None:
                 Nuevo.padre = padre
                 padre.derecha = Nuevo
+                if aux>self.h:
+                    self.h = aux 
+                Nuevo.h = aux
                 return True
 
             elif Nuevo.valor > padre.valor:
@@ -43,6 +65,13 @@ class ArbolBinario():
             else:
                 self.size -= 1
                 return False
+            aux += 1
+
+
+    def insertar(self, valores): #eficiencia O(N*h)
+        for valor in valores: #O(N)
+            self._insertarAux(valor) #O(h)
+
 
     def busque(self, valor):
         nodo = self.raiz
@@ -55,17 +84,20 @@ class ArbolBinario():
                 return False
         return nodo    
 
+
     def PreOrden(self,raiz):
         if self.len == 0:
             return
 
-        print(raiz.valor)
+        self._listaRetorno+=[raiz.valor]
         if raiz.izquierda != None:
             self.PreOrden(raiz.izquierda)
 
         if raiz.derecha != None:
             self.PreOrden(raiz.derecha)
-
+        
+        
+        return self._listaRetorno
 
     def EnOrden(self,raiz):
         if self.len == 0:
@@ -74,10 +106,26 @@ class ArbolBinario():
         if raiz.izquierda != None:
             self.PreOrden(raiz.izquierda)
 
-        print(raiz.valor)
+        self._listaRetorno+=[raiz.valor]
 
         if raiz.derecha != None:
             self.PreOrden(raiz.derecha)
+        
+        return self._listaRetorno
+    
+    def EnOrdenInv(self,raiz):
+        if self.len == 0:
+            return
+
+        if raiz.derecha != None:
+            self.EnOrdenInv(raiz.derecha)
+
+        self._listaRetorno+=[raiz.valor]
+
+        if raiz.izquierda != None:
+            self.EnOrdenInv(raiz.izquierda)
+
+        return self._listaRetorno
 
     def PosOrden(self,raiz):
         if self.len == 0:
@@ -89,15 +137,60 @@ class ArbolBinario():
         if raiz.derecha != None:
             self.PreOrden(raiz.derecha)
 
-        print(raiz.valor)
+        self._listaRetorno+=[raiz.valor]
+
+        
+        return self._listaRetorno
+    
+    def contarhojas(self,raiz):
+        if self.len == 0:
+            return
+
+        if raiz.derecha ==None and raiz.izquierda == None:
+            self._hojas += 1
+
+        if raiz.izquierda != None:
+            self.contarhojas(raiz.izquierda)
+
+        if raiz.derecha != None:
+            self.contarhojas(raiz.derecha)
+        return self._hojas
+    
+    def contarHijos(self,raiz):
+        if self.len == 0:
+            return
+
+        if (raiz.derecha == None) ^ (raiz.izquierda == None):
+            self._hijos += 1
+
+        if raiz.izquierda != None:
+            self.contarHijos(raiz.izquierda)
+
+        if raiz.derecha != None:
+            self.contarHijos(raiz.derecha)
+        return self._hijos
+    
+    def VerificarRacimo(self,raiz):
+        if self.len == 0:
+            return 
+
+        if self.h == raiz.h:
+            self._racimo += 1 
+        if raiz.izquierda != None:
+            self.VerificarRacimo(raiz.izquierda)
+
+        if raiz.derecha != None:
+            self.VerificarRacimo(raiz.derecha)
+        return 2**(self.h-1) == self._racimo
 
 
-a = ArbolBinario()
-a.insertar(1)
-a.insertar(2)
-a.insertar(3)
-print(a.busque(3))
-a.PreOrden(a.raiz)
+
+a = ArbolBinario([30, 20])
+a.insertar([40, 10, 50, 25, 35])
+
+print(a.PreOrden(a.raiz))
+
+
 
 
         
